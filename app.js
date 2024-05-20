2a6568afb359b27a889eb6d39562dd6a9360f80a
Zainab Murtaza <162459926+Zainab090@users.noreply.github.com>
2024-05-20 20:41:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//Assignment#1
+alert("Welcome to this website");
+
+//Assignment#2
+alert("JavaScript Alert\n\nError! Please enter a valid password.");
+
+//Assignment#3
+alert("JavaScript Alert\n\nWelcome to JS Land... \nHappy Coding!");
+
+//Assignment#4
+alert("JavaScript Alert\n\nWelcome to JS Land");
+alert("JavaScript Alert\n\nprevent this page from creating additional dialogs");
+
+//Assignment#5
+alert("JavaScript Alert\n\nHello...I can run JS through my web browser's console");
+
+//Assignment#6
